0de0db20dd39dd02e64ed45944539725e1414e48
dineshkumarv151098 <68579672+dineshkumarv151098@users.noreply.github.com>
2020-07-26 20:30:05 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print("hi       welcome to       letsupgrade")
+
+
+# In[9]:
+
+
+"""(@) (@)
+      ^
+     ===
+     """
+
+
+# In[11]:
+
+
+print("he's the person i'm searching for!")
+
+
+# In[13]:
+
+
+print('he\'s the person i\'m searching for!')
+
+
+# In[28]:
+
+
+name = "dinesh"
+dob = "15.10.1998"
+place = "hosur"
+print("my name is ", name,"and my DOB is ",dob,"i'm living in ",place)
+
+
+# In[73]:
+
+
+a = int(input("enter the value for a= "))
+b = int(input("enter the value for b= "))
+if a==b:
+    print ("number cannot be same")
+if a<=0:
+    print("please the the valid number ")
+if b<=0:
+    print("please the the valid number ")
+if a>0:
+    if b>0:
+        if a!=b:
+            print(a**b)
+            print(a*b)
+            print(a/b)
+            print(a%b)
+            print(a+b)
+            print(a-b)
+            print(a//b)
+            print(a|b)
+            print(a&b)
+            print(a^b)
+            print(a<<b)
+            print(a>>b)
+            
+            
+            
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
