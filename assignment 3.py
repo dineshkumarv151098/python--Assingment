5e755f81144725eeee1b6b030139aae806c2d9c7
dineshkumarv151098 <68579672+dineshkumarv151098@users.noreply.github.com>
2020-07-28 19:42:33 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+n = int(input("enter the value "))
+sum = 0
+while (n > 0):
+    sum += n
+    n-=1
+print ("the sum of n numbers is  ", sum)
+
+
+# In[16]:
+
+
+num = int(input("Enter a number: "))
+if num > 1:
+for each in range(2,num):
+if (num % each) == 0:
+print(num,"is not a prime number")
+print(each,"times",num//each,"is",num)
+
+
+# In[ ]:
+
+
+
+
